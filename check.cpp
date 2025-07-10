061578eeae3fda85ceba0040cce2f8afe7c80762
SahajW <sahaj1224@student.ku.edu.np>
2025-07-10 22:25:51 +0545

    Add files via upload

--- check.cpp
+++ check.cpp
@@ -1,4 +1,6 @@
 #include "game.hpp"
+#include "manual.hpp"
+#include "player.hpp"
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <iostream>
@@ -7,17 +9,17 @@
 
 
 
-enum class Scene { Menu, Game };
+enum class Scene { Menu, Game, Manual };
 
 using namespace sf;
 using namespace std;
 
 void fadeIn(sf::RenderWindow& window, sf::Sprite& background,sf::Sprite& start,sf::Sprite& credits,sf::Sprite& quit,
-    sf::Sprite& volume, sf::Music& music) {
+    sf::Sprite& volume, sf::Music& music,sf::Sprite& manual,sf::Sprite& menu) {
     sf::RectangleShape overlay(sf::Vector2f(window.getSize()));
     overlay.setFillColor(sf::Color(0, 0, 0, 255)); // Fully black
 
-    const float fadeDuration = 2.0f; // seconds
+    const float fadeDuration = 1.0f; // seconds
     sf::Clock clock;
 
     music.play(); // Start music just before fade-in
@@ -35,6 +37,8 @@ void fadeIn(sf::RenderWindow& window, sf::Sprite& background,sf::Sprite& start,s
         window.draw(credits);        // draw credits button
         window.draw(quit);           // draw quit button
         window.draw(volume);         // draw volume button
+        window.draw(manual);         // draw manual button
+        window.draw(menu);           // draw menu button
         window.draw(overlay);        // then draw fading black overlay
         window.display();
     }
@@ -48,14 +52,20 @@ int main()
     sf::VideoMode videoMode({1000, 1400}, 32);                          // setting the window size and bits
     sf::RenderWindow window(videoMode, "LEAP UP!", sf::Style::Default); // creates name for window and creates close, resize, and minimize button
 
-    sf::Texture background, start, credits, quit, volume, mute;
+    sf::Texture background, start, credits, quit, volume, mute,manual,start2,manual2,credits2,exit2,menu;
 
-    if (!background.loadFromFile("assets/bg.png") ||
-        !start.loadFromFile("assets/start.png") ||
+    if (!background.loadFromFile("assets/bg1.png") ||
+        !start.loadFromFile("assets/play.png") ||
         !credits.loadFromFile("assets/credits.png") ||
         !quit.loadFromFile("assets/quit.png") ||
         !volume.loadFromFile("assets/volume.png") ||
-        !mute.loadFromFile("assets/mute.png"))
+        !mute.loadFromFile("assets/mute.png")||
+        !start2.loadFromFile("assets/play2.png")||
+        !manual.loadFromFile("assets/manual.png")||
+        !manual2.loadFromFile("assets/manual2.png")||
+        !credits2.loadFromFile("assets/credits2.png")||
+        !exit2.loadFromFile("assets/exit2.png")||
+        !menu.loadFromFile("assets/menu.png")) 
     {
         return -1; // error loading image
     }
@@ -65,9 +75,15 @@ int main()
     sf::Sprite Credit(credits);
     sf::Sprite Quit(quit);
     sf::Sprite Volume(volume);
+    sf::Sprite Manual(manual);
+    sf::Sprite Start2(start2);
+    sf::Sprite Manual2(manual2);
+    sf::Sprite Credits2(credits2);
+    sf::Sprite Exit2(exit2);
+    sf::Sprite Menu(menu); 
 
     sf::Music music;//music sprite
-    if (!music.openFromFile("assets/music1.mp3"))
+    if (!music.openFromFile("assets/music.mp3"))
     {
         std::cerr << "Failed to load music\n";
         return -1;
@@ -77,21 +93,26 @@ int main()
     
 
     bool musicOn = true;
- 
-    Start.setPosition({230.f, 400.f});
-    Credit.setPosition({230.f, 600.f});
-    Quit.setPosition({230.f, 800.f});
-
+    Menu.setPosition({275.f, 220.f}); // Set the position of the menu background
+    Start.setPosition({240.f, 350.f});
+    Manual.setPosition({240.f, 550.f});
+    Credit.setPosition({240.f, 750.f});
+    Quit.setPosition({240.f, 950.f});
+    Start2.setPosition({240.f,350.f});
+    
     Volume.setScale({0.2f, 0.2f});
-    Start.setScale({0.85f, 0.85f});
-    Credit.setScale({0.85f, 0.85f});
-    Quit.setScale({0.85f, 0.85f});
-
+    Menu.setScale({0.8f, 0.8f});
+    Start.setScale({0.6f, 0.6f});
+    Manual.setScale({0.6f, 0.6f});
+    Credit.setScale({0.6f, 0.6f});
+    Quit.setScale({0.6f, 0.6f});
+    Start2.setScale({0.6f,0.6f});
+    
     Scene currentScene = Scene::Menu;
-    fadeIn(window, bg,Start,Credit,Quit,Volume, music); // Fade in effect
+    fadeIn(window, bg,Start,Credit,Quit,Volume, music,Manual,Menu); // Fade in effect
 
-
-
+    
+    
     while (window.isOpen())
     { // Loop to hold the window
 
@@ -106,17 +127,50 @@ int main()
             {
                 window.close();
             }
+            
+        
 
-            /*if (event.is<sf::Event::MouseMoved>())
+            //Hover effect for buttons
+            if (event.is<sf::Event::MouseMoved>())
             {
                 const auto &mouse = event.getIf<sf::Event::MouseMoved>();
                 sf::Vector2f mousePos(static_cast<float>(mouse->position.x), static_cast<float>(mouse->position.y));
+                
 
+                //Start hover
                 if (Start.getGlobalBounds().contains(mousePos))
                 {
-                    Start.setColor(sf::Color(255, 255, 255, 150));//hover effect for start button
+                    Start.setTexture(start2);//hover effect for start button
                 }
-            }*/
+                else{
+                    Start.setTexture(start); // reset to original texture  
+                }
+                //manual hover    
+                if (Manual.getGlobalBounds().contains(mousePos))
+                {
+                    Manual.setTexture(manual2); //hover effect for manual button
+                }
+                else{
+                    Manual.setTexture(manual); // reset to original texture
+                }
+                //credits hover
+                if (Credit.getGlobalBounds().contains(mousePos))
+                {
+                    Credit.setTexture(credits2); //hover effect for credits button
+                }
+                else{
+                    Credit.setTexture(credits); // reset to original texture
+                }
+                //quit hover
+                if (Quit.getGlobalBounds().contains(mousePos))
+                {
+                    Quit.setTexture(exit2); //hover effect for quit button
+                }
+                else{
+                    Quit.setTexture(quit); // reset to original texture
+                }
+               
+            }
             if (event.is<sf::Event::MouseButtonPressed>())
             {
                 const auto &mouse = event.getIf<sf::Event::MouseButtonPressed>();
@@ -131,6 +185,12 @@ int main()
                     currentScene = Scene::Game;
                     runGame(window); // Start the game
                 }
+                if(Manual.getGlobalBounds().contains(mousePos))
+                {
+                    currentScene = Scene::Manual;
+                    runManual(window); // Open manual
+                }
+
 
 
                 if (Volume.getGlobalBounds().contains(mousePos))
@@ -152,10 +212,12 @@ int main()
 
              
             
-            window.clear(sf::Color(255, 165, 0)); // color
+            window.clear(sf::Color(0, 0, 0)); // color
             
             window.draw(bg);
+            window.draw(Menu); 
             window.draw(Start);
+            window.draw(Manual);
             window.draw(Credit);
             window.draw(Quit);
             window.draw(Volume);
